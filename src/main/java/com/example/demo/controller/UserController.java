528486f98ace383937af236f3de21845b36825a0
ailun123456789 <1127676786@qq.com>
2020-06-09 23:21:45 +0800

    项目初始化

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/list")
+    @ResponseBody
+    public String getUserList() {
+        System.out.println("西西飒撒旦？旦 啊是大angwen ");
+        return ",这才是你们说你笨部署啊:1111";
+    }
+}
