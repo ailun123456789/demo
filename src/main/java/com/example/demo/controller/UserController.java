a38ce85d1995cde66068da7de4fcd2ed87726686
ailun123456789 <42951759+ailun123456789@users.noreply.github.com>
2020-10-17 21:24:50 +0800

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
     @GetMapping("/list")
     @ResponseBody
     public String getUserList() {
-        System.out.println("西西飒撒旦？旦 啊是大angwen ");
+        System.out.println("jiuzhe? ");
         System.out.println("ssndoiqawdnfio");
         return ",这才是你们说你笨部署啊:1111";
         System.out.println("this is my change");
